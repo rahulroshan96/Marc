ee28b294f5ab5542fa371fe2a73b62b657b602e3
rahulroshan96 <rahulroshan96@gmail.com>
2016-04-05 16:39:26 +0530

    update

---
+++ ping.h
@@ -0,0 +1,24 @@
+#ifndef ns_ping_h
+#define ns_ping_h
+
+#include "agent.h"
+#include "tclcl.h"
+#include "packet.h"
+#include "address.h"
+#include "ip.h"
+
+
+struct hdr_ping {
+  char ret;
+  double send_time;
+};
+
+
+class PingAgent : public Agent {
+ public:
+  PingAgent();
+  int command(int argc, const char*const* argv);
+  void recv(Packet*, Handler*);
+ protected:
+  int off_ping_;
+};
